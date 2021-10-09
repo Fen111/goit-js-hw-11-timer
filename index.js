bd2e0b32b38b3255bf2ffb8e103b03f014dc884b
Viktor Sinelnykov <Fenya.ua@gmail.com>
2021-10-09 15:51:57 +0300

    таймер обратного отсчета

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-// const startTime = new Date('Jul 17, 2019');
-// const currentTime = Date.now();
-// console.log(startTime - currentTime);
-// console.log(currentTime - startTime);
+import refs from './refs.js';
 
-const refs = {
-  days: document.querySelector('span[data-value="days"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  mins: document.querySelector('span[data-value="mins"]'),
-  secs: document.querySelector('span[data-value="secs"]'),
-};
+const { days, hours, mins, secs } = refs;
 
 class CountdownTimer {
-  constructor({ onTick }) {
+  constructor({ targetDate, selector, onTick }) {
+    this.targetDate = targetDate;
+    this.intervalId = null;
+    this.deltaTime = 0;
     this.onTick = onTick;
+    this.selector = selector;
   }
+
   start() {
-    const startTime = new Date('Jul 17, 2019');
+    this.intervalId = setInterval(() => {
+      let currentDate = Date.now();
+      this.deltaTime = this.targetDate - currentDate;
+      const time = this.getTimeComponents(this.deltaTime);
 
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-
-      // const findTime = deltaTime - (2 * 0 + 1) * 1000;
-
-      // console.log(findTime);
-      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
-
-      console.log(`${days}:${hours}:${mins}:${secs}`);
-      this.onTick({ days, hours, mins, secs });
+      this.onTick(time);
     }, 1000);
   }
-
   pad(value) {
     return String(value).padStart(2, '0');
   }
@@ -49,51 +38,15 @@ class CountdownTimer {
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('jan 01, 2022'),
   onTick: updateClockface,
 });
 
+timer.start();
+
 function updateClockface({ days, hours, mins, secs }) {
-  refs.days.textContent = `${days}:`;
-  refs.hours.textContent = `${hours}:`;
-  refs.mins.textContent = `${mins}:`;
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
   refs.secs.textContent = `${secs}`;
 }
-
-timer.start.bind(timer);
-
-// const timer = {
-//   start() {
-//     const startTime = new Date('Jul 17, 2019');
-
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startTime;
-
-//       // const findTime = deltaTime - (2 * 0 + 1) * 1000;
-
-//       // console.log(findTime);
-//       const { days, hours, mins, secs } = getTimeComponents(deltaTime);
-
-//       console.log(`${days}:${hours}:${mins}:${secs}`);
-//       updateClockface({ days, hours, mins, secs });
-//     }, 1000);
-//   },
-// };
-
-// timer.start();
-
-// function pad(value) {
-//   return String(value).padStart(2, '0');
-// }
-
-// function getTimeComponents(time) {
-//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//   const hours = pad(
-//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-//   );
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-//   return { days, hours, mins, secs };
-// }
